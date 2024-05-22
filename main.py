f86a355397ad757be1d68cf7c58fe0e1a4d36abe
Nandhitha Kamal <nandhitha@infraspec.dev>
2024-05-22 17:12:40 +0530

    listen only for messages on DM channels

--- main.py
+++ main.py
@@ -28,12 +28,17 @@ async def on_ready():
 
 @discordClient.event
 async def on_message(message):
-    if message.author == discordClient.user :
-        return
-    else:
-        print(message)
-        print(type(message.channel))
-        print("forwarding message to chatpgt")
-        completion_response = forward_user_message(message.content)
-        await message.channel.send(completion_response.choices[0].message.content)
+        # todo: nest properly after debugging
+        if (type(message.channel) != discord.channel.DMChannel):
+            return
+        if message.author == discordClient.user:
+            return
+        else:
+            completion_response = forward_user_message(message.content)
+            await message.channel.send(completion_response.choices[0].message.content)
+            return
+
+
+
+
 discordClient.run(os.getenv('DISCORD_BOT_TOKEN'))
